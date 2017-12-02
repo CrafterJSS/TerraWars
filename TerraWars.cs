9c8a36dde50b69c856daa15615b600459e66783f
Jonathan Svensson <racingjss@hotmail.com>
2017-12-02 14:46:04 -0500

    First Commit

---
+++ TerraWars.cs
@@ -0,0 +1,17 @@
+﻿using Terraria.ModLoader;
+
+namespace TerraWars
+{
+    class TerraWars : Mod
+    {
+        public TerraWars()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
