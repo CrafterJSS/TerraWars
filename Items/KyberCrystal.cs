6ba58db73731a3a6530b3b48b3f6b4c94870b557
CrafterJSS <racingjss@hotmail.com>
2018-04-14 14:03:25 -0400

    Kyber Crystal and Blue Lightsaber additions

---
+++ Items/KyberCrystal.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+// A blank Kyber Crystal, powerful things which, among other things, are used in the creation of lightsabers.
+
+namespace TerraWars.Items
+{
+    public class KyberCrystal : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Kyber Crystal");
+            Tooltip.SetDefault("A blank Kyber Crystal. Clear and shiny!");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 100;
+            item.value = Item.buyPrice(0, 50, 0, 0);
+            item.rare = 12;
+        }
+    }
+}
--- Items/LightsaberBlue.cs
+++ Items/LightsaberBlue.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -29,6 +31,7 @@ namespace TerraWars.Items
             item.autoReuse = true;
         }
 
+        // Recipe
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
@@ -36,6 +39,28 @@ namespace TerraWars.Items
             recipe.AddTile(TileID.WorkBenches);
             recipe.SetResult(this);
             recipe.AddRecipe();
+        }
+
+        // The Glowmask
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+        {
+            Texture2D texture = mod.GetTexture("Items/LightsaberBlue_Glowmask");
+            spriteBatch.Draw
+            (
+                texture,
+                new Vector2
+                (
+                    item.position.X - Main.screenPosition.X + item.width * 0.5f,
+                    item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
+                ),
+                new Rectangle(0, 0, texture.Width, texture.Height),
+                Color.Blue,
+                rotation,
+                texture.Size() * 0.5f,
+                scale,
+                SpriteEffects.None,
+                0f
+            );
         }
     }
 }
